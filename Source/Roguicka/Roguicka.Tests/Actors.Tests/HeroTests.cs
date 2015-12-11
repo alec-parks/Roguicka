755ed6b0e2a6291be992f0b8f2fbe4a3f73734fc
Alexander Parks <alec.parks@gmail.com>
2015-12-11 16:18:20 -0600

    Healing and not dead tests
    Added tests for the Heal method and IsDead method.

--- Source/Roguicka/Roguicka.Tests/Actors.Tests/HeroTests.cs
+++ Source/Roguicka/Roguicka.Tests/Actors.Tests/HeroTests.cs
@@ -5,6 +5,14 @@ namespace Roguicka.Tests.Actors.Tests
 {
     public class HeroTests
     {
+        [Fact]
+        public void ShouldNotBeDead()
+        {
+            var sut = new Hero(1,1,10,10,'@');
+
+            Assert.False(sut.IsDead());
+        }
+
         [Theory]
         [InlineData(0)]
         [InlineData(-5)]
@@ -13,6 +21,26 @@ namespace Roguicka.Tests.Actors.Tests
             var sut = new Hero(1, 1, hp, 10, '@');
 
             Assert.True(sut.IsDead());
+        }
+
+        [Theory]
+        [InlineData(2, 5, 7)]
+        [InlineData(10, 10, 20)]
+        public void ShouldHeal(int start, int heal, int result)
+        {
+            var sut = new Hero(1, 1, start, start + heal, '@');
+            sut.Heal(heal);
+            Assert.Equal(result,sut.CurrentHP);
+        }
+
+        [Theory]
+        [InlineData(2, 10, 10)]
+        [InlineData(10, 100000, 10)]
+        public void ShouldNotHealAboveMaxHp(int start, int heal, int max)
+        {
+            var sut = new Hero(1, 1, start, max, '@');
+            sut.Heal(heal);
+            Assert.Equal(max,sut.CurrentHP);
         }
     }
 }
