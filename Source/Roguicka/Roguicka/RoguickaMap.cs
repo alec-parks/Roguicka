62fe64778306f15d1b6298d9d4244af0c6beb772
Alexander Parks <alec.parks@gmail.com>
2015-12-28 14:40:05 -0600

    Map wrap take 3
    Actually include the changes to the wrapper...

--- Source/Roguicka/Roguicka/RoguickaMap.cs
+++ Source/Roguicka/Roguicka/RoguickaMap.cs
@@ -7,179 +7,190 @@ namespace Roguicka
     public class RoguickaMap :IMap
     {
 
-        public IMap Map { get; set; }
+        private readonly Map _map;
+
+        public RoguickaMap()
+        {
+            _map = new Map();
+        }
+
+        public RoguickaMap(Map map)
+        {
+            _map = map;
+        }
 
         public void Initialize(int width, int height)
         {
-            Map.Initialize(width, height);
+            _map.Initialize(width, height);
         }
 
         public bool IsTransparent(int x, int y)
         {
-            return Map.IsTransparent(x, y);
+            return _map.IsTransparent(x, y);
         }
 
         public bool IsWalkable(int x, int y)
         {
-            return Map.IsWalkable(x, y);
+            return _map.IsWalkable(x, y);
         }
 
         public bool IsInFov(int x, int y)
         {
-            return Map.IsInFov(x, y);
+            return _map.IsInFov(x, y);
         }
 
         public bool IsExplored(int x, int y)
         {
-            return Map.IsExplored(x, y);
+            return _map.IsExplored(x, y);
         }
 
         public void SetCellProperties(int x, int y, bool isTransparent, bool isWalkable, bool isExplored = false)
         {
-            Map.SetCellProperties(x, y, isTransparent, isWalkable, isExplored);
+            _map.SetCellProperties(x, y, isTransparent, isWalkable, isExplored);
         }
 
         public void Clear(bool isTransparent = false, bool isWalkable = false)
         {
-            Map.Clear(isTransparent, isWalkable);
+            _map.Clear(isTransparent, isWalkable);
         }
 
         public IMap Clone()
         {
-            return Map.Clone();
+            return _map.Clone();
         }
 
         public void Copy(IMap sourceMap, int left = 0, int top = 0)
         {
-            Map.Copy(sourceMap, left, top);
+            _map.Copy(sourceMap, left, top);
         }
 
         public void ComputeFov(int xOrigin, int yOrigin, int radius, bool lightWalls)
         {
-            Map.ComputeFov(xOrigin, yOrigin, radius, lightWalls);
+            _map.ComputeFov(xOrigin, yOrigin, radius, lightWalls);
         }
 
         public void AppendFov(int xOrigin, int yOrigin, int radius, bool lightWalls)
         {
-            Map.AppendFov(xOrigin, yOrigin, radius, lightWalls);
+            _map.AppendFov(xOrigin, yOrigin, radius, lightWalls);
         }
 
         public IEnumerable GetAllCells()
         {
-            return Map.GetAllCells();
+            return _map.GetAllCells();
         }
 
         public IEnumerable GetCellsAlongLine(int xOrigin, int yOrigin, int xDestination, int yDestination)
         {
-            return Map.GetCellsAlongLine(xOrigin, yOrigin, xDestination, yDestination);
+            return _map.GetCellsAlongLine(xOrigin, yOrigin, xDestination, yDestination);
         }
 
         public IEnumerable GetCellsInRadius(int xOrigin, int yOrigin, int radius)
         {
-            return Map.GetCellsInRadius(xOrigin, yOrigin, radius);
+            return _map.GetCellsInRadius(xOrigin, yOrigin, radius);
         }
 
         public IEnumerable GetCellsInArea(int xOrigin, int yOrigin, int distance)
         {
-            return Map.GetCellsInArea(xOrigin, yOrigin, distance);
+            return _map.GetCellsInArea(xOrigin, yOrigin, distance);
         }
 
         public IEnumerable GetBorderCellsInRadius(int xOrigin, int yOrigin, int radius)
         {
-            return Map.GetBorderCellsInRadius(xOrigin, yOrigin, radius);
+            return _map.GetBorderCellsInRadius(xOrigin, yOrigin, radius);
         }
 
         public IEnumerable GetBorderCellsInArea(int xOrigin, int yOrigin, int distance)
         {
-            return Map.GetBorderCellsInArea(xOrigin, yOrigin, distance);
+            return _map.GetBorderCellsInArea(xOrigin, yOrigin, distance);
         }
 
         public IEnumerable GetCellsInRows(params int[] rowNumbers)
         {
-            return Map.GetCellsInRows(rowNumbers);
+            return _map.GetCellsInRows(rowNumbers);
         }
 
         public IEnumerable GetCellsInColumns(params int[] columnNumbers)
         {
-            return Map.GetCellsInColumns(columnNumbers);
+            return _map.GetCellsInColumns(columnNumbers);
         }
 
         public Cell GetCell(int x, int y)
         {
-            return Map.GetCell(x, y);
+            return _map.GetCell(x, y);
         }
 
         public string ToString(bool useFov)
         {
-            return Map.ToString(useFov);
+            return _map.ToString(useFov);
         }
 
         public MapState Save()
         {
-            return Map.Save();
+            return _map.Save();
         }
 
         public void Restore(MapState state)
         {
-            Map.Restore(state);
+            _map.Restore(state);
         }
 
         public Cell CellFor(int index)
         {
-            return Map.CellFor(index);
+            return _map.CellFor(index);
         }
 
         public int IndexFor(int x, int y)
         {
-            return Map.IndexFor(x, y);
+            return _map.IndexFor(x, y);
         }
 
         public int IndexFor(Cell cell)
         {
-            return Map.IndexFor(cell);
+            return _map.IndexFor(cell);
         }
 
         IEnumerable<Cell> IMap.GetAllCells()
         {
-            return Map.GetAllCells();
+            return _map.GetAllCells();
         }
 
         IEnumerable<Cell> IMap.GetCellsAlongLine(int xOrigin, int yOrigin, int xDestination, int yDestination)
         {
-            return Map.GetCellsAlongLine(xOrigin, yOrigin, xDestination, yDestination);
+            return _map.GetCellsAlongLine(xOrigin, yOrigin, xDestination, yDestination);
         }
 
         IEnumerable<Cell> IMap.GetCellsInRadius(int xOrigin, int yOrigin, int radius)
         {
-            return Map.GetCellsInRadius(xOrigin, yOrigin, radius);
+            return _map.GetCellsInRadius(xOrigin, yOrigin, radius);
         }
 
         IEnumerable<Cell> IMap.GetCellsInArea(int xOrigin, int yOrigin, int distance)
         {
-            return Map.GetCellsInArea(xOrigin, yOrigin, distance);
+            return _map.GetCellsInArea(xOrigin, yOrigin, distance);
         }
 
         IEnumerable<Cell> IMap.GetBorderCellsInRadius(int xOrigin, int yOrigin, int radius)
         {
-            return Map.GetBorderCellsInRadius(xOrigin, yOrigin, radius);
+            return _map.GetBorderCellsInRadius(xOrigin, yOrigin, radius);
         }
 
         IEnumerable<Cell> IMap.GetBorderCellsInArea(int xOrigin, int yOrigin, int distance)
         {
-            return Map.GetBorderCellsInArea(xOrigin, yOrigin, distance);
+            return _map.GetBorderCellsInArea(xOrigin, yOrigin, distance);
         }
 
         IEnumerable<Cell> IMap.GetCellsInRows(params int[] rowNumbers)
         {
-            return Map.GetCellsInRows(rowNumbers);
+            return _map.GetCellsInRows(rowNumbers);
         }
 
         IEnumerable<Cell> IMap.GetCellsInColumns(params int[] columnNumbers)
         {
-            return Map.GetCellsInColumns(columnNumbers);
+            return _map.GetCellsInColumns(columnNumbers);
         }
 
-        public int Width => Map.Width;
-        public int Height => Map.Height;
+        public int Width => _map.Width;
+        public int Height => _map.Height;
+
     }
 }
