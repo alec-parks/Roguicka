f355230ac16e7715cc125814338642fc05f865fc
Alexander Parks <alec.parks@gmail.com>
2016-03-04 10:45:33 -0600

    Game Singleton
    Added Game singleton to store and serve map and font data.
    
    Might remove font data...

---
+++ Source/Roguicka/Roguicka/Game.cs
@@ -0,0 +1,18 @@
+﻿using Roguicka.Maps;
+
+namespace Roguicka
+{
+  public class Game
+  {
+    private static Game _instance;
+    public RoguickaMap Map { get; set; }
+    public string FontFile { get; set; }
+
+    private Game(){}
+
+    public static Game Instance
+    {
+      get { return _instance ?? (_instance = new Game()); }
+    }
+  }
+}
