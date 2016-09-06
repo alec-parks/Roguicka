2897e5f3814cd3450d462b34e7f0b82d8423eef4
Alec Parks <alexander.parks@here.com>
2016-09-06 14:49:30 -0500

    Initial Logging Engine
    Added initial logging engine. Messages are added manually to the list
    then the WriteMessages should be called to display the output (currently
    10 messages, configurable later).

---
+++ Source/Roguicka/Roguicka/Engines/LoggingEngine.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using RLNET;
+
+namespace Roguicka.Engines
+{
+    public class LoggingEngine
+    {
+        private readonly List<string> _messages = new List<string>();
+        private int messageCount = 10;
+
+        public void WriteMessages(RLConsole rlConsole)
+        {
+            var lines = 0;
+            for (var i = _messages.Count; lines < messageCount && i > 0; i--)
+            {
+                rlConsole.Print(0, lines, _messages[i-1],RLColor.Black);
+                lines++;
+            }
+        }
+
+        public void AddMessage(string message)
+        {
+            _messages.Add(message);
+        }
+    }
+}
